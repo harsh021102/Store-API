d4287846fb6de8ae2e4546d44b791daff6511085
harsh021102 <harsh021102@gmail.com>
2024-01-05 21:30:18 +0530

    TC:- 04:50

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -2,14 +2,21 @@ const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({
-        name: '2',
-    })
-    res.status(200).json({products, nbHits:products.length})
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(7)
+    .skip(3)
+
+    // .skip(3)- skips first 3 elements
+
+    // const products = await Product.find({}).select('name price') use d to select some specific fields
+    // name: {$regex: search, $options: 'i'} //options i means case insensitive
+    res.status(200).json({nbHits:products.length,products})
 }
 const getAllProducts = async (req,res) => {
     // console.log(req.query);
-    const { featured,company,search } = req.query
+    const { featured,company,name, sort, fields } = req.query
     const queryObject = {}
     if(featured)
     {
@@ -17,13 +24,37 @@ const getAllProducts = async (req,res) => {
     }
     if(company)
     {
-        queryObject.featured = company
+        queryObject.company = company
     }
-    if(search)
-        queryObject.featured = search
+    if(name)
+    {
+        // console.log(name);
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
 
-    console.log(queryObject);
-    const products = await Product.find(req.query)
+    // console.log(queryObject);
+    let result = Product.find(queryObject)
+    //sort
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createAt')
+        //if no value is specified then it will be sorted accordng to createdAt field
+    }
+
+    if(fields)
+    {
+        const fieldList = fields.split(',').join(' ');
+        result = result.select(fieldList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req,query.limit) || 10
+    const skip = (page-1) * limit;
+    result = result.skip(skip).limit(limit)
+    const products = await result
     res.status(200).json({products, nbHits:products.length})
 }
 
